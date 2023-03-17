e634b1bde82def12efaab17f1166fb50dd48656b
Ianhaag1 <119625942+Ianhaag1@users.noreply.github.com>
2023-03-17 13:03:46 -0400

    Add files via upload

---
+++ LetConst.js
@@ -0,0 +1,10 @@
+const PI = 3.14
+PI = 42; //error
+
+// you cannot redeclare a variable using Let. Let creates a 'block space'
+
+// Same with Const
+
+// you can reassign let but you cannot reassign const (constant)
+
+// hoisting is using the compiler. Variables can be hoisted to the top of the scope or block space.
